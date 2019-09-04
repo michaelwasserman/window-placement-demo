2076716f1d8c345db64724eeafa5079016f7e37c
Mike Wasserman <msw@chromium.org>
2019-09-04 14:51:36 -0700

    Refine demo

--- service-worker.js
+++ service-worker.js
@@ -6,67 +6,17 @@ self.addEventListener('activate', event => {
   console.log("MSW service worker activate");
 });
 
-addEventListener('fetch', event => {
-  event.waitUntil(async function() {
-    // Exit early if we don't have access to the client.
-    // Eg, if it's cross-origin.
-    if (!event.clientId) return;
-
-    // Get the client.
-    const client = await clients.get(event.clientId);
-    // Exit early if we don't get the client.
-    // Eg, if it closed.
-    if (!client) return;
-
-    // Send a message to the client.
-    client.postMessage({
-      msg: "Hey I just got a fetch from you!",
-      url: event.request.url
-    });
-   
-  }());
-});
-
-
 self.addEventListener('message', function(event){
   console.log("MSW service worker message: " + event.data);
-
   // TODO(msw): event.waitUntil?
-  event.waitUntil(async function() {
-    // Exit early if we don't have access to the client.
-    if (!event.clientId) return;
-
-    // Get the client.
-    const client = await clients.get(event.clientId);
-    // Exit early if we don't get the client.
-    // Eg, if it closed.
-    if (!client) return;
-
-    // Send a message to the client.
-    client.postMessage({
-      msg: "Hey I just got a fetch from you!",
-      url: event.request.url
-    });
-  })
-
-
   displays = getDisplays().then(async function(displays) {
-    if (event.data === "show-displays-button") {
-      console.log("MSW posting message to client...");
+    if (event.data.sender === "show-displays-button") {
+      // console.log("MSW posting message to client...");
       event.ports[0].postMessage(displays);
-      // if (!event.clientId) {
-      //   console.log("MSW unknown client");
-      //   return;
-      // }
-      // const client = await clients.get(event.clientId);
-      // console.log("MSW posting message to client: " + client);
-      // client.postMessage({ msg: "update displays", displays: displays });
-    } else if (event.data === "open-window-button") {
+    } else if (event.data.sender === "open-window-button") {
       open_window(event);
-    } else if (event.data === "present-slides-button") {
-      present_slides(event, displays);
-    } else if (event.data === "open-dashboard-button") {
-      open_dashboard(event);
+    } else if (event.data.sender === "present-slide-button") {
+      present_slide(event, displays);
     }
   })
 });
@@ -77,31 +27,30 @@ self.addEventListener('notificationclick', function(event) {
   open_window(event);
 });
 
-var getDisplays = async function() {
-  console.log("MSW getDisplays");
+async function getDisplays() {
+  // console.log("MSW getDisplays");
   const displays = await navigator.screen.requestDisplays();
-  console.log("MSW displays: " + displays.length);
+  // console.log("MSW displays: " + displays.length);
   // for (const display of displays) {
-  //   console.log("name: " + display.name);
-  //   console.log("scaleFactor: " + display.scaleFactor);
-  //   console.log("width: " + display.width);
-  //   console.log("height: " + display.height);
-  //   console.log("left: " + display.left);
-  //   console.log("top: " + display.top);
-  //   console.log("colorDepth: " + display.colorDepth);
-  //   console.log("isPrimary: " + display.isPrimary);
-  //   console.log("isInternal: " + display.isInternal);
+  //   console.log(`[${i}] '${display.name}' ${display.left},${display.top} ${display.width}x${display.height} ${display.isPrimary ? '(Primary)': ''}`);
+  //   console.log(`scaleFactor:${display.scaleFactor}, colorDepth:${display.colorDepth} isPrimary:${display.isPrimary}, isInternal:${display.isInternal}`);
   // }
   return displays;
 }
 
 function open_window(event) {
-  const promise = clients.openWindow('https://google.com',
-                                     { x:100, y:100, width:300, height:300, type:"window"});
+  var url = 'https:wikipedia.org'
+  var options = { x:50, y:100, width:400, height:200, type:"window"}
+  if (event && event.data && event.data.url.length != 0)
+    url = event.data.url;
+  if (event && event.data && event.data.options)
+    options = event.data.options;
+  console.log("MSW Calling openWindow(" + url + "," + JSON.stringify(options) + ")");
+  const promise = clients.openWindow(url, options);
   // event.waitUntil(promise);
 }
 
-function present_slides(event, displays) {
+function present_slide(event, displays) {
   // var slide_options = { x:screen.left(), y:screen.top(), width:screen.availWidth(), height:screen.availHeight()-200, type:"window"};
   // var notes_options = { x:screen.left(), y:screen.availHeight()-200, width:screen.availWidth(), height:200, type:"window"};
   var slide_options = { x:10, y:10, width:800, height:600, type:"window"};
@@ -110,21 +59,13 @@ function present_slides(event, displays) {
     slide_options = { x:displays[1].left, y:displays[1].top, width:displays[1].width, height:displays[1].height, type:"window"};
     notes_options = { x:displays[0].left, y:displays[0].top, width:displays[0].width, height:displays[0].height, type:"window"};
   }
-  console.log("MSW slide: " + slide_options.x + "," + slide_options.y + " " + slide_options.width + "x" + slide_options.height);
-  console.log("MSW notes: " + notes_options.x + "," + notes_options.y + " " + notes_options.width + "x" + notes_options.height);
-  const slide_promise = clients.openWindow('https://google.com',
-                                   slide_options);
-  //'data:text/html;charset=utf-8,<head><title>Presentation: First Slide</title></head><body><h1>Window Placement API Demo</h1><p><p/><h2>msw@chromium.org & staphany@chromium.org</h2></body>',
-  // //'data:text/html;charset=utf-8,<head><title>Presentation (Speaker Notes)</title></head><body><h3>Speaker Notes</h3><p></p>Thanks for watching our Window Placement API demo!</body>',
-  //event.waitUntil(slide_promise);
-  const notes_promise = clients.openWindow('https://cnn.com',
-                                   notes_options);
-  // // event.waitUntil(notes_promise);
-}
-
-function open_dashboard(event) {
-  const presentation = clients.openWindow('data:text/html;charset=utf-8,<head><title>Presentation: First Slide</title></head><body><h1>Window Placement API Demo</h1><p><p/><h2>msw@chromium.org & staphany@chromium.org</h2></body>',
-                                          { x:0, y:0, width:800, height:600, type:"window"});
+  // console.log("MSW slide: " + slide_options.x + "," + slide_options.y + " " + slide_options.width + "x" + slide_options.height);
+  // console.log("MSW notes: " + notes_options.x + "," + notes_options.y + " " + notes_options.width + "x" + notes_options.height);
+  const slide_promise = clients.openWindow('slide.html', slide_options);
+  // event.waitUntil(slide_promise);
+  // TODO(msw): opening another window clobbers the first... 
+  // const notes_promise = clients.openWindow('https://cnn.com', notes_options);
+  // event.waitUntil(notes_promise);
 }
 
 function cautious_open_window(event) {
